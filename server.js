903801407f3f8906db1fbbe793fbdf3da46fe990
dasaint001 <elijahbamidele48@yahoo.com>
2023-03-30 06:16:10 +0100

    data validation added

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Email = require('mongoose-type-email')
 const User = require('./models/userModel')
 const app = express()
 
 app.use(express.json())
 
 //this is a test api for the checks
-app.get('/', (req, res)=> {
+app.get('/', (req, res, next)=> {
     res.status(200).json({ 
         message: true,
         data: 'it works'
@@ -18,11 +19,23 @@ app.post('/user', async(req, res) => {
 
     try{
         const user = await User.create(req.body)
-        res.status(201).json({ 
-            status: true,
-            data: user
-        })
+        const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
+        if (typeof req.body.firstName != "string" || typeof req.body.lastName != "string") {
+            res.status(400).json({message: "one of the names is not a string"})
+            console.log('This is not string');
+        }
+        if(emailRegexp.test(req.body.email) == false){
+            res.status(400).json({message})
+            console.log('This is not a valid email');
+        }
+        else{
+            { res.status(201).json({ 
+                status: true,
+                data: user
+            })}
+        }
+       
     }catch (error){
         console.log(error.message);
         res.status(500).json({message: error.message})
